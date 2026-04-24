c0bd91218f26621a3fc2f48aa30ec3ef67600b88
Munchunnce <vimalkumarka478@gmail.com>
2026-04-24 17:09:35 +0530

    products all data fetch

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,18 +7,27 @@ const Products = () => {
     const fetchProducts = async () => {
       const res = await fetch('https://fakestoreapiserver.reactbd.org/api/products');
       const data = await res.json()
-      setProducts(data);
-      console.log(data);
+      setProducts(data.data);
+      console.log(data.data);
     };
 
     fetchProducts();
   }, []);
 
   return (
-    <div>
-      
+    <div className='productsWrapper'>
+      {
+        products.map((product) => (
+          <div className='card' key={product._id}>
+            <img src={product.image} alt="" />
+            <h4>{product.title}</h4>
+            <h5>{product.price}</h5>
+            <button className='btn'>Add to cart</button>
+          </div>
+        ))
+      }
     </div>
   )
 }
 
-export default Products
+export default Products;
