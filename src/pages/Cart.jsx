5c846824b45b8d6c01f4a6c7fd7bf8e06be89337
Munchunnce <vimalkumarak478@gmail.com>
2026-04-25 15:46:39 +0530

    cart fetch all data

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { remove } from '../store/cartSlice';
 
 const Cart = () => {
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleDelete = (productId) => {
+    dispatch(remove(productId));
+  };
+
   return (
     <div>
-      This is Cart page...
-    </div>
-  )
-}
+      <h3>Cart items</h3>
 
-export default Cart
+      <div className="cartWrapper">
+        {products.length === 0 ? (
+          <h4>Cart is empty</h4>
+        ) : (
+          products.map((product) => (
+            <div className="cartCard" key={product.id}>
+              <img src={product.image} alt="" />
+              <h5>{product.title}</h5>
+
+              {/* quantity logic later add karenge */}
+              <button className="btn">-</button>
+              <span>1</span>
+              <button className="btn">+</button>
+
+              <button
+                onClick={() => handleDelete(product.id)}
+                className="btn"
+              >
+                Delete
+              </button>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
