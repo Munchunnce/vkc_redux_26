4f83e77314a8256891494424046941aacb9dc05e
Munchunnce <vimalkumarak478@gmail.com>
2026-04-30 21:43:15 +0530

    Provider and PresistGate store data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 // import './App.css';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import store, { persistor } from './store/store';
 import Home from './pages/Home';
 import Products from './components/Products';
 import Cart from './pages/Cart';
 import Navbar from './components/Navbar';
+import { PersistGate } from 'redux-persist/integration/react'; // PersistGate import
 
 
 
@@ -13,7 +14,8 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <BrowserRouter>
+        <PersistGate loading={null} persstor={persistor}>
+          <BrowserRouter>
           <Navbar/>
           <Routes>
             <Route path='/' element={<Home/>}/>
@@ -21,6 +23,7 @@ function App() {
             <Route path='/cart' element={<Cart/>}/>
           </Routes>
         </BrowserRouter>
+        </PersistGate>
       </Provider>
     </div>
   );
