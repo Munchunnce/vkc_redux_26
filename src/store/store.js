db5b49e17a8021f6f07cfc10a77a712384b6f73b
Munchunnce <vimalkumarak478@gmail.com>
2026-04-30 21:29:59 +0530

    import presiste

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice';
 import productReducer from './productSlice';
+// Redux Persist Imports
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage
+import { combineReducers } from "redux";
 
 
 const store = configureStore({
